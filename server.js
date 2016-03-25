8a8da08f2326a66fc411206549f09dfa4a70f6e6
Rohit Rai <rohitkrai03@gmail.com>
2016-03-25 23:26:11 +0530

    Added proper routes for get and post of data from a JSON file

--- server.js
+++ server.js
@@ -1,34 +1,76 @@
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
 var bodyParser = require('body-parser');
+var app = express();
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+var DATA_FILE = path.join(__dirname, 'data.json');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
 });
 
-// more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+router.route('/data')
+    .get(function(req, res){
+        fs.readFile(DATA_FILE, function(err, data){
+            if(err) {
+                console.error(err);
+                process.exit(1);
+            }
+            res.json(JSON.parse(data));
+        });
+    })
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+    .post(function(req, res){
+        fs.readFile(DATA_FILE, function(err, data){
+            if(err) {
+                console.error(err);
+                process.exit(1);
+            }
+            var data = JSON.parse(data);
+            // NOTE: In a real implementation, we would likely rely on a database or
+            // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+            // treat Date.now() as unique-enough for our purposes.
+            var newData = {
+                id: Date.now(),
+                author: req.body.author,
+                text: req.body.text
+            }
+            data.push(newData);
+            fs.writeFile(DATA_FILE, JSON.stringify(data, null, 4), function(err){
+                if (err) {
+                    console.error(err);
+                    process.exit(1);
+                }
+                res.json(comments);
+            });
+        });
+    });
+
+app.use('/api', router)
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
